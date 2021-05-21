e6577395bd8e4d44f2b12ee5cd156dda4cbdcb3a
Ali Shirinfeshan <sh201220@gmail.com>
2021-05-21 18:38:01 +0430

    Upload to GitHub

---
+++ MyPortfolio/Models/Portfolio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models
+{
+    public class Portfolio
+    {
+        // اطلاعات مربوط به بخش نمونه کار ها
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public string Image { get; set; }
+
+      
+    }
+}
