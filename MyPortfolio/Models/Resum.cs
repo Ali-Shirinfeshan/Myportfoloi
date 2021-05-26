ecaef2014e3a898a017025bc1f6402edd9c4476c
Ali Shirinfeshan <sh201220@gmail.com>
2021-05-26 11:31:48 +0430

    Move Resum To ViewComponent

---
+++ MyPortfolio/Models/Resum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models
+{
+    public class Resum
+    {
+        private string v;
+        private object p;
+
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Date { get; set; }
+        public string Discription { get; set; }
+        public string Location { get; set; }
+
+        public Resum(string title, string subtitle, string date, string discription, string location)
+        {
+            Title = title;
+            Subtitle = subtitle;
+            Date = date;
+            Discription = discription;
+            Location = location;
+        }
+
+        public Resum(string v, object p)
+        {
+            this.v = v;
+            this.p = p;
+        }
+    }
+}
