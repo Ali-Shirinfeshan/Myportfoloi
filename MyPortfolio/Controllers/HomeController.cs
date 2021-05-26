95fb1d5e1edd997e5c97912b9bd455b4f0ed3f5c
Ali Shirinfeshan <sh201220@gmail.com>
2021-05-26 20:14:52 +0430

    Move Portfolio to viewcomponent Move Services to partial view Added Persion font

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -1,48 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyPortfolio.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyPortfolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<Portfolio> _portfolios = new List<Portfolio>
-        {
-            new Portfolio
-            {
-                Name="اولین نمونه کار من", Discription="ایجاد قالب 404 به عنوان اولین نمونه کار من ", Image="portfolio-1.jpg"
-            },
-             new Portfolio
-            {
-                Name="اپلیکیشن نیرو کارت", Discription="اپلیکشن وزارت نیرو که شامل خدمات بانکی همراه با باشگاه مشتریان می باشد", Image="portfolio-2.jpg"
-            },
-              new Portfolio
-            {
-                Name="اپلیکیشن پارسی کارت", Discription="اپلیکشن سایر ارگان ها که شامل خدمات بانکی همراه با باشگاه مشتریان می باشد", Image="portfolio-3.jpg"
-            },
-               new Portfolio
-            {
-                Name="پروژه دانشکده مهندسی", Discription="پروژه وب سایت فروشگاهی   ", Image="portfolio-4.jpg"
-            },
-                new Portfolio
-            {
-                Name="پروژه وب سایت مخابرات", Discription="پنل گزارش گری و ثبت گزارشات کارشناسان اینترنت مخابرات ", Image="portfolio-5.jpg"
-            },
-                 new Portfolio
-            {
-                Name="وب سایت شادلین", Discription="وب سایت گردشگری و تفریحی کیش ", Image="portfolio-6.jpg"
-            },
-                  new Portfolio
-            {
-                Name=" اپلیکیشن املاک", Discription="اپلیکیشن درج ملک جهت رهن ،اجاره ، خرید و فروش ", Image="portfolio-7.jpg"
-            }
-
-        };
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -52,16 +16,8 @@ namespace MyPortfolio.Controllers
 
         public IActionResult Index()
         {
-
-            ViewData["Address"] = "Iran,Fars,Shiraz Model Street";
-            ViewData["Email"] = "Sh201220@gmail.com";
-            ViewData["Mobile"] = "+98-9374181503";
-            return View(_portfolios);
-
+            return View();
         }
-
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- MyPortfolio/Models/Portfolio.cs
+++ MyPortfolio/Models/Portfolio.cs
@@ -11,7 +11,17 @@ namespace MyPortfolio.Models
         public string Name { get; set; }
         public string Discription { get; set; }
         public string Image { get; set; }
+        public string Type { get; set; }
 
-      
+        public Portfolio(string name, string discription, string image,string type)
+        {
+            Name = name;
+            Discription = discription;
+            Image = image;
+            Type=type;
+        }
+
     }
+
+
 }
